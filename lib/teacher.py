a512dd67f25e66b73e2f4b4ed110e310ea5208cd
Dan <jacobydan01@gmail.com>
2024-05-16 14:24:26 -0400

    inheritance1

--- lib/teacher.py
+++ lib/teacher.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python
+knowledge = [
+    "str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",
+]
 
 from user import User
 
@@ -6,5 +16,11 @@ import random
 
 class Teacher(User):
 
+    #pulls attributes from parent
+    def __init__(self, first_name, last_name, knowledge = knowledge):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge
+
+    #picks random element from list to "teach"
     def teach(self):
-        pass
+        return self.knowledge[random.randint(0,(len(knowledge) - 1))]
